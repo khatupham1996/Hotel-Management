5de515dc96ac53d6db8b3645362d3534c5db9850
khatupham1996 <khatupham96@gmail.com>
2025-10-23 14:29:36 -0400

    Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Dashboard from "./pages/Dashboard";
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          {/* <Route path="bookings" element={<Bookings />} />
+          <Route path="bookings/:bookingId" element={<Booking />} />
+          <Route path="checkin/:bookingId" element={<Checkin />} />
+          {/* <Route path="checkout/:bookingId" element={<Checkout />} /> */}
+          {/* <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
