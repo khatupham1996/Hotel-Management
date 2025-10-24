75eb7661dbd6be5cbe0e391a016bb23a33f29d1f
khatupham1996 <khatupham96@gmail.com>
2025-10-23 22:10:11 -0400

    Cabins

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://lbmjovoszchvxjfxxwus.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxibWpvdm9zemNodnhqZnh4d3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNTEwOTAsImV4cCI6MjA3NjgyNzA5MH0.HiUHBxKNdq0i2mQ_rnNq_fDzI5vNzbNDSerwjKWRQO4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
